91da847f9cd937830d4efd58bbc1aa2333d61838
tphanson <tphanson@kambria.io>
2021-02-24 11:38:51 +0700

    parallelize dataset

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ from env import OhmniInSpace
 from criterion import ExpectedReturn
 from helper.utils import parse_experiences
 
+tf.debugging.set_log_device_placement(True)
+
 
 # Environment
 train_env = OhmniInSpace.env()
@@ -42,15 +44,15 @@ replay_buffer.collect_steps(
     steps=initial_collect_steps
 )
 
-with tf.device('/GPU:1'):
-    def map_fn(experiences, info):
-        start_policy_state, end_policy_state = agent._hidden_states(
-            experiences)
-        step_types, start_state, action, rewards, end_state = parse_experiences(
-            experiences, agent._pre_n_steps, agent._n_steps)
-        experiences = (step_types, start_state, start_policy_state,
-                       action, rewards, end_state, end_policy_state)
-        return experiences, info
+
+def map_fn(experiences, info):
+    start_policy_state, end_policy_state = agent._hidden_states(
+        experiences)
+    step_types, start_state, action, rewards, end_state = parse_experiences(
+        experiences, agent._pre_n_steps, agent._n_steps)
+    experiences = (step_types, start_state, start_policy_state,
+                   action, rewards, end_state, end_policy_state)
+    return experiences, info
 
 
 dataset = replay_buffer.as_dataset().map(map_fn).prefetch(2)
