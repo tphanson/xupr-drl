a8930e8d4b818b841f21c94ce85d462559a8843d
tphanson <tphanson@kambria.io>
2021-03-02 20:35:27 +0700

    refine reward func

---
+++ debug.py
@@ -0,0 +1,11 @@
+gamma = 0.99
+positive_reward = 10
+negative_reward = -10
+
+for i in range(500):
+    positive_reward = gamma * positive_reward - 0.025
+for i in range(500):
+    negative_reward = gamma * negative_reward - 0
+
+print('positive_reward', positive_reward)  # -19.802885508727464
+print('negative_reward', negative_reward)  # -19.934295169575766
