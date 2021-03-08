4bfa575be3e83b778bfd78d1b6cc88bdc0ba8f72
Tu Phan <tphanson@kambria.io>
2021-03-08 14:33:20 +0700

    curriculum learning

--- env/OhmniInSpace.py
+++ env/OhmniInSpace.py
@@ -229,8 +229,6 @@ class PyEnv(py_environment.PyEnvironment):
             return False, -0.1
         # Ohmni on his way
         # return False, -0.05 + cosine_sim / 20
-        if cosine_sim < 0:
-            return False, -0.05
         return False, cosine_sim / 20
 
     def _reset(self):
