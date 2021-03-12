e587a4288cddd5d0728b0d9a1764ea7fa6a75410
Tu Phan <tphanson@kambria.io>
2021-03-12 11:51:09 +0700

    curriculum learning

--- train.py
+++ train.py
@@ -32,15 +32,19 @@ replay_buffer = per.PrioritizedExperienceRelay(
 )
 
 # Init buffer
-# random_policy = random_tf_policy.RandomTFPolicy(
-#     time_step_spec=agent.time_step_spec,
-#     action_spec=agent.action_spec,
-#     policy_state_spec=agent.policy_state_spec,
-# )
-random_policy = agent
+random_policy = random_tf_policy.RandomTFPolicy(
+    time_step_spec=agent.time_step_spec,
+    action_spec=agent.action_spec,
+    policy_state_spec=agent.policy_state_spec,
+)
+baseline = agent
 replay_buffer.collect_steps(
     train_env, random_policy,
-    steps=initial_collect_steps
+    steps=int(initial_collect_steps/2)
+)
+replay_buffer.collect_steps(
+    train_env, baseline,
+    steps=int(initial_collect_steps/2)
 )
 
 
